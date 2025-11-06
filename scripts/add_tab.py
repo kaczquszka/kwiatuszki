5924411546b16ef9a320e6d4fccae601e50f942d
Marta <martakotowska20@gmail.com>
2025-11-06 16:59:12 +0100

    porzadki

--- scripts/add_tab.py
+++ scripts/add_tab.py
@@ -1,4 +1,4 @@
- 
+#skrypt dodaje taba na koniec kazdej linijki w answers.csv zeby mozna bylo odczytac jako tsv
 with open("answers.csv", "r") as infile, open("answers_tab.csv", "w") as outfile:
     for line in infile:
         line = line.rstrip('\n')
