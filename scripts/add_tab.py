1dd161ac1d59a934c85663d7c5fb62dacf2ab37c
Marta <martakotowska20@gmail.com>
2025-11-08 15:53:28 +0100

    updated dataset a little, added notebooks with predictions

--- scripts/add_tab.py
+++ scripts/add_tab.py
@@ -1,7 +1,8 @@
 #skrypt dodaje taba na koniec kazdej linijki w answers.csv zeby mozna bylo odczytac jako tsv
-with open("answers.csv", "r") as infile, open("answers_tab.csv", "w") as outfile:
+with open("../datasets/answers.csv", "r") as infile, open("../datasets/answers_tab.csv", "w") as outfile:
     for line in infile:
         line = line.rstrip('\n')
         spliited = line.rsplit(None,1)
         line = "\t".join(spliited)
         outfile.write(line+"\n")
+       
