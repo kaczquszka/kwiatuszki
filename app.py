75b8a4098ce5b3721f9bc511791d6d1668e8dbbd
Marta <martakotowska20@gmail.com>
2025-12-20 22:41:16 +0100

    f

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ if st.session_state.step == 1:
             st.warning('fill all of the fileds, please :)')
         else:
             st.session_state.step=2
-            st.empty()
+            form_placeholder.empty()
             st.rerun()        
         
 elif st.session_state.step == 2:  
